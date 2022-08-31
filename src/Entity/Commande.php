6e2e67feb6978a3728c8e189c1c5ff5a4bc1fec1
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-08-31 15:00:45 +0200

    Initial commit

---
+++ src/Entity/Commande.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommandeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommandeRepository::class)
+ */
+class Commande
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Membre::class, inversedBy="commandes")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $id_membre;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Produit::class, inversedBy="commandes")
+     */
+    private $id_produit;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $quantite;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $etat;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_enregistrement;
+
+    public function __construct()
+    {
+        $this->id_produit = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdMembre(): ?Membre
+    {
+        return $this->id_membre;
+    }
+
+    public function setIdMembre(?Membre $id_membre): self
+    {
+        $this->id_membre = $id_membre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Produit>
+     */
+    public function getIdProduit(): Collection
+    {
+        return $this->id_produit;
+    }
+
+    public function addIdProduit(Produit $idProduit): self
+    {
+        if (!$this->id_produit->contains($idProduit)) {
+            $this->id_produit[] = $idProduit;
+        }
+
+        return $this;
+    }
+
+    public function removeIdProduit(Produit $idProduit): self
+    {
+        $this->id_produit->removeElement($idProduit);
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $date_enregistrement): self
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+}
