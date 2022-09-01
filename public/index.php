4a231fdd561d5098262c99285c6e284b96f09ab9
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-09-01 12:30:51 +0200

    Update NavBar

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     //return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    // Ajout du setting du TimeZone
     return new Kernel($context['APP_ENV'],
                 (bool) $context['APP_DEBUG'],
                 date_default_timezone_set( 'Europe/Paris'));
