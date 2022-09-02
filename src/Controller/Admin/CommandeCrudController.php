3ad64d917ae3ba88e68012cd1d236b512fb4ab22
HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>
2022-09-03 01:51:39 +0200

    Add DELETE ADD-BUTTON COMMANDE
    Signed-off-by: HOTTIN Eric <70805292+EHNWeb@users.noreply.github.com>

--- src/Controller/Admin/CommandeCrudController.php
+++ src/Controller/Admin/CommandeCrudController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Commande;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
@@ -35,5 +37,12 @@ class CommandeCrudController extends AbstractCrudController
             DateTimeField::new('date_enregistrement')->onlyOnIndex(),
         ];
     }
-  
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::DELETE)
+        ;
+    }
+
 }
